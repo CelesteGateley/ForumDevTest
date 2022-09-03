b7496268e58bfb271f8e0d1fa512606dc2dc5286
Celeste Gateley <celeste.gateley@gmail.com>
2022-09-03 23:53:06 +0100

    Added repository link to Forum model

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Repositories\ForumRepository;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,11 @@ class Forum extends Model
     use HasFactory;
 
     protected $guarded = ['id','created_at','updated_at'];
+    private ForumRepository $repository;
+
+    public function repository(): ForumRepository
+    {
+        isset($this->repository) || $this->repository = new ForumRepository($this);
+        return $this->repository;
+    }
 }
