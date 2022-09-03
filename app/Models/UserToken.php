4ccf2a4ccb22d4bc5e46142a97ffe67acc65873c
Celeste Gateley <celeste.gateley@gmail.com>
2022-09-04 00:23:30 +0100

    Implemented UserTokens

---
+++ app/Models/UserToken.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserToken extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'token';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $casts = ['expiry' => 'integer'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getExpiresAttribute(): Carbon
+    {
+        return Carbon::createFromTimestamp($this->expiry);
+    }
+
+    public function setExpiresAttribute(Carbon $expires): void
+    {
+        $this->expiry = $expires->unix();
+        $this->save();
+    }
+
+    public function expired(): bool
+    {
+        return $this->exires->before(now());
+    }
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Models\UserToken;
+use App\Repositories\Abstracts\ModelRepository;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Str;
+
+class UserRepository extends ModelRepository
+{
+    private User $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public static function create(array $data): Model
+    {
+        return User::create($data);
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public static function all(): Collection|array
+    {
+        return User::all();
+    }
+
+    public function get(): User
+    {
+        return $this->user;
+    }
+
+    public function update(array $data): User
+    {
+        $this->user->update($data);
+        $this->save();
+        return $this->get();
+    }
+
+    public function save(): bool
+    {
+        return $this->user->save();
+    }
+
+    public function delete(): bool
+    {
+        return $this->user->delete();
+    }
+
+    public function saved(): bool
+    {
+        return isset($this->user->id);
+    }
+
+    public function refresh(): UserRepository
+    {
+        $this->user->refresh();
+        return $this;
+    }
+
+    public static function getUserFromToken(string $token): ?User
+    {
+        $userToken = UserToken::find($token);
+        if (!isset($userToken) || $userToken->expired()) return null;
+        return $userToken->user;
+    }
+
+    public function getToken(int $expires = 60*60): UserToken
+    {
+        $token = $this->user->tokens->first();
+        if (!isset($token) || $token->expired()) {
+            $token = $this->generateToken($expires);
+        }
+        return $token;
+    }
+
+    private function generateToken(int $expires): UserToken
+    {
+        do {
+            $token = Str::random(64);
+            $found = UserToken::find($token);
+        } while (isset($found));
+        $userToken = UserToken::make([
+            'token' => $token,
+            'expiry' => now()->addSeconds($expires)->unix(),
+        ]);
+        $this->user->tokens()->save($userToken);
+        return $userToken;
+    }
+}
